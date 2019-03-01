e3d012af6e027bba4a9746d640259e173056fc85
scott.snelgrove1@gmail.com <scott.snelgrove1@gmail.com>
2019-03-01 14:29:37 -0800

    this is a test

--- nemean_lion/script.sh
+++ nemean_lion/script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-apt-get update && apt-get upgrade
+apt-get update && apt-get upgrade -y
 apt-get install openssh-server -y
 PORT_NUMBER="9123"
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-sed -i "s/^Port .*/Port $PORT_NUMBER/g" /etc/ssh/sshd_config
+sed -i "s/#Port 22/Port $PORT_NUMBER/g" /etc/ssh/sshd_config
 echo "New sshport $PORT_NUMBER"
 /etc/init.d/ssh restart
+service sshd status
